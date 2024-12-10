2fefb7266d65ca7a172bde4f677e193b244058c2
devramoss <devramoss2003@gmail.com>
2024-12-10 20:31:08 -0300

    Convertendo coordenada em graus para utm

--- Conversor/Controllers/CoordenadaController.cs
+++ Conversor/Controllers/CoordenadaController.cs
@@ -8,22 +8,17 @@ namespace Conversor.Controllers
     [Route("[controller]")]
     public class CoordenadaController : ControllerBase
     {
-        [HttpPost]
+        [HttpPost("converter-graus")]
 
         public IActionResult ConverterParaGraus([FromBody] Coordenada coordenada)
         {
             try
             {
-                string Zona = coordenada.Zona;
-                double Leste = coordenada.Leste;
-                double Norte = coordenada.Norte;
-
-
-                if(string.IsNullOrEmpty(Zona) || Leste <= 0 || Norte <= 0)
-                    return BadRequest(new { error = "Dados inválidos. Certifique-se de fornecer zone, easting e northing." });
+                if (string.IsNullOrEmpty(coordenada.Zona) || !coordenada.Leste.HasValue || !coordenada.Norte.HasValue)
+                    return BadRequest(new { error = "Dados inválidos. Certifique-se de fornecer os campos zona, leste e norte." });
 
                 Coordinate coordenadaGraus = new Coordinate();
-                UniversalTransverseMercator utm = new UniversalTransverseMercator(Zona, Leste, Norte);
+                UniversalTransverseMercator utm = new UniversalTransverseMercator(coordenada.Zona, coordenada.Leste.Value, coordenada.Norte.Value);
                 coordenadaGraus = UniversalTransverseMercator.ConvertUTMtoLatLong(utm);
 
                 return Ok(new
@@ -40,5 +35,33 @@ namespace Conversor.Controllers
             }
         }
 
+        [HttpPost("converter-utm")]
+        public IActionResult ConverterParaUtm([FromBody] Coordenada coordenada)
+        {
+            try
+            {
+                if (!coordenada.Latitude.HasValue || !coordenada.Longitude.HasValue)
+                    return BadRequest(new { error = "Dados inválidos. Certifique-se de fornecer os campos de latitude e longitude!" });
+
+                Coordinate coordenadaGraus = new Coordinate(coordenada.Latitude.Value, coordenada.Longitude.Value);
+                UniversalTransverseMercator utm = coordenadaGraus.UTM;
+
+                return Ok(new
+                {
+                    zona = utm.LongZone + utm.LatZone,
+                    leste = utm.Easting,
+                    norte = utm.Northing
+                });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    error = "Erro interno no servidor.",
+                    details = ex.Message
+                });
+            }
+
+        }
     }
 }
--- Conversor/Models/Coordenada.cs
+++ Conversor/Models/Coordenada.cs
@@ -2,8 +2,10 @@
 {
     public class Coordenada
     {
-        public double Leste { get; set; }
-        public double Norte { get; set; }
-        public string ?Zona { get; set; }
+        public double ?Leste { get; set; }
+        public double ?Norte { get; set; }
+        public string ? Zona { get; set; }
+        public double ? Latitude { get; set; }
+        public double ? Longitude { get; set; }
     }
 }
